2a8e7ebdf7f4231b403cacc249b2b067088cce15
Naidr <adrianrodrigueznaranjo@alumno.ieselrincon.es>
2023-02-14 22:44:07 +0000

    g

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ xhttp.send();
 
 function showResult(xml, value) {
   var array = ["//game_name","//figure_name","/shops/shop/games/game[3]/game_name","//figure_name | //game_name",
-"//game[last()]//node()","//figure[1]//text()","count(//games/*)","//game[/platform/game_priece <20]",
+"//game[last()]//node()","//figure[1]//text()","count(//games)","//game[/platform/game_priece <20]",
 "//figure_priece[. > 60]","/shops/shop/games/game/platform/platform_name"];
   var txt = "";
   path = array[value];
