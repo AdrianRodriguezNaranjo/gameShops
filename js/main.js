c759425f0040d6fe101106a601f2907c77dc6f27
Naidr <adrianrodrigueznaranjo@alumno.ieselrincon.es>
2023-02-14 20:21:56 +0000

    a

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ xhttp.open("GET", "gameShop.xml", true);
 xhttp.send();
 
 function showResult(xml, value) {
-  var array = ["//game_name","//figure_names","//game_name[/game_priece>40]"]
+  var array = ["//game_name","//figure_name","//game_name[game_priece>40]"]
   var txt = "";
   path = array[value];
   if (xml.evaluate) {
